60ad86acb42d87b3181c3440b4a3de7d445a0d97
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2026-01-04 16:26:12 -0300

    chore: alteração de atributo;

--- src/interfaces/Authenticable.java
+++ src/interfaces/Authenticable.java
@@ -3,6 +3,6 @@ package interfaces;
 /**Interaface de padronização de métodos de autenticação de usuário. @author @MrErykCardoso.*/
 public interface Authenticable {
     abstract String getCpf();
-    abstract boolean authenticate(String password);
-    abstract boolean getHierarchy();
+    abstract boolean authenticate(String auth);
+    abstract int getHierarchy();
 }
