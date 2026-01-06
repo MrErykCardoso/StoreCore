911754c4afcfc964c0372961f391db5405b30a77
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2026-01-06 20:44:25 -0300

    chore: elaboração de modelos de produto;

--- src/models/product_models/HardwareProduct.java
+++ src/models/product_models/HardwareProduct.java
@@ -1,5 +1,12 @@
 package models.product_models;
+import abstract_classes.Enterprise;
+import abstract_classes.StockProduct;
 
-public class HardwareProduct {
-    
+public class HardwareProduct extends StockProduct{
+    public HardwareProduct(String name, String description, double price, double priceWithDiscount, Enterprise provider, String code, int inStock){
+        super(name, description, price, priceWithDiscount, provider, code, inStock);
+    }
+    public HardwareProduct(String id, String name, String description, double price, double priceWithDiscount, Enterprise provider, String code, int inStock){
+        super(id, name, description, price, priceWithDiscount, provider, code, inStock);
+    }
 }
--- src/models/product_models/Repair.java
+++ src/models/product_models/Repair.java
@@ -1,5 +1,13 @@
 package models.product_models;
 
-public class Repair {
-    
+import abstract_classes.Enterprise;
+import abstract_classes.Service;;
+
+public class Repair extends Service{
+    public Repair(String name, String description, double price, double priceWithDiscount, Enterprise provider, String startDate, String endDate){
+        super(name, description, price, priceWithDiscount, provider, startDate, endDate);
+    }
+    public Repair(String id, String name, String description, double price, double priceWithDiscount, Enterprise provider, String startDate, String endDate){
+        super(id, name, description, price, priceWithDiscount, provider, startDate, endDate);
+    }
 }
