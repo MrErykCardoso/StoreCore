eb947cc90ed8edcd2c60c1d836b4c70e26e4e195
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2026-01-06 20:34:06 -0300

    chore: elaboração do modelo de fornecedor;

--- src/models/enterprise_models/Provider.java
+++ src/models/enterprise_models/Provider.java
@@ -1,5 +1,11 @@
 package models.enterprise_models;
+import abstract_classes.Enterprise;
 
-public class Provider {
-    
+public class Provider extends Enterprise{
+    public Provider(String name, String cnpj, String email, String phoneNumber){
+        super(name, cnpj, email, phoneNumber);
+    }
+    public Provider(String id, String name, String cnpj, String email, String phoneNumber){
+        super(id, name, cnpj, email, phoneNumber);
+    }
 }
